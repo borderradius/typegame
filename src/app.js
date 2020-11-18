baa84dc4488d9aa6a20791c25a6bf9e7cf02b797
spacemonkey <spacemonkey925@gmail.com>
2020-11-18 14:10:50 +0900

    router 추가

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
-import { sum } from './math.js';
 import style from '../style.css';
-import { getList } from './model/List';
+// import { getList } from './model/List';
 
-console.log(sum(1, 2))
-try {
-  console.log(getList())
-} catch (e) {
-  console.error(e);
+
+import {initialRoutes,historyRouterPush} from './router.js'
+
+const historyAppDiv = document.querySelector('#history-app')
+
+// Browser History
+initialRoutes('history', historyAppDiv)
+
+window.onload = () => {
+  const historyLinker = document.querySelectorAll('span.history')
+
+  historyLinker.forEach(el => {
+    el.addEventListener('click', (evt) => {
+      const pathName = evt.target.getAttribute('route')
+
+      historyRouterPush(pathName, historyAppDiv)
+    })
+  })
 }
-
-
---
+++ src/router.js
@@ -0,0 +1,37 @@
+import About from './pages/about.html';
+import Home from './pages/home.html';
+
+const routes = {
+  '/': Home,
+  '/home': Home,
+  '/about': About
+}
+
+// entry point
+function initialRoutes (mode, el) {
+  renderHTML(el, routes['/'])
+
+  if (mode === 'history') {
+    window.onpopstate = () => renderHTML(el, routes[window.location.pathname])
+  } else {
+    window.addEventListener('hashchange', () => {
+      return renderHTML(el, getHashRoute())
+    })
+  }
+}
+
+// set browser history
+function historyRouterPush (pathName, el) {
+  window.history.pushState({}, pathName, window.location.origin + pathName)
+  renderHTML(el, routes[pathName])
+}
+
+// render
+function renderHTML (el, route) {
+  el.innerHTML = route
+}
+
+export {
+  initialRoutes,
+  historyRouterPush
+}
